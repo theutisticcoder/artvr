432c5f7817f2defa043c8cb414213fabd64d2f2e
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-09-04 18:27:00 -0700

    fix response

--- index.js
+++ index.js
@@ -5,27 +5,30 @@ const { Client, Environment, ApiError } = require('square');
 const app = express();
 app.use(bodyParser.json());
 
-const squareClient = new Client({
+const client = new Client({
     environment: Environment.Production, // or Environment.Production
     accessToken: 'EAAAlvd0owabWAkzKXlgauXdesy8ahHbIdJFMODM7leb7Z_SdXJv1RaQ84ZeuGmz',
 });
 
-app.post('/create-payment', async (req, res) => {
-    try {
-      const response = await squareClient.paymentsApi.createPayment({
-        sourceId: 'cnon:card-nonce-ok',
-        idempotencyKey: '6f32f7e1-f234-4c8e-9b32-42f7f3cb69fb',
-        amountMoney: {
-          amount: 15,
-          currency: 'USD'
-        }
-      });
+try {
+  const response = await client.paymentsApi.createPayment({
+    sourceId: 'ccof:GaJGNaZa8x4OgDJn4GB',
+    idempotencyKey: '7b0f3ec5-086a-4871-8f13-3c81b3875218',
+    amountMoney: {
+      amount: 15,
+      currency: 'USD'
+    },
+    autocomplete: true,
+    customerId: 'W92WH6P11H4Z77CTET0RNTGFW8',
+    locationId: 'LMJY97S69RNWK',
+    referenceId: '123456',
+    note: 'lakerise'
+  });
 
-      console.log(response.result);
-    } catch(error) {
-      console.log(error);
-    }
-});
+  console.log(response.result);
+} catch(error) {
+  console.log(error);
+}
 app.use(express.static(__dirname))
 app.use(express.static("/create-payment"))
 app.listen(3000, () => {
