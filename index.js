61d8da04fa40dbfbbadfd87e77b76a7f26da7c4d
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-09-04 18:38:01 -0700

    fix index.js

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ const client = new Client({
     accessToken: 'EAAAlvd0owabWAkzKXlgauXdesy8ahHbIdJFMODM7leb7Z_SdXJv1RaQ84ZeuGmz',
 });
 
-try {
-  const response = await client.paymentsApi.createPayment({
-    sourceId: 'ccof:GaJGNaZa8x4OgDJn4GB',
-    idempotencyKey: '7b0f3ec5-086a-4871-8f13-3c81b3875218',
-    amountMoney: {
-      amount: 15,
-      currency: 'USD'
-    },
-    autocomplete: true,
-    customerId: 'W92WH6P11H4Z77CTET0RNTGFW8',
-    locationId: 'LMJY97S69RNWK',
-    referenceId: '123456',
-    note: 'lakerise'
+app.post('/create-payment', async (req, res) => {
+  try {
+    const response = await client.paymentsApi.createPayment({
+      sourceId: 'ccof:GaJGNaZa8x4OgDJn4GB',
+      idempotencyKey: '7b0f3ec5-086a-4871-8f13-3c81b3875218',
+      amountMoney: {
+        amount: 15,
+        currency: 'USD'
+      },
+      autocomplete: true,
+      customerId: 'W92WH6P11H4Z77CTET0RNTGFW8',
+      locationId: 'LMJY97S69RNWK',
+      referenceId: '123456',
+      note: 'lakerise'
+    }); 
+  } catch(err){
+    console.log(err);
+  }
   });
-
-  console.log(response.result);
-} catch(error) {
-  console.log(error);
-}
 app.use(express.static(__dirname))
 app.use(express.static("/create-payment"))
 app.listen(3000, () => {
