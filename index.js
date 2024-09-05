b5dbf9ac0041cb84440e97c5492724344db6109c
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-09-04 18:53:10 -0700

    fix index.js

--- index.js
+++ index.js
@@ -13,20 +13,17 @@ const client = new Client({
 app.post('/create-payment', async (req, res) => {
   try {
     const response = await client.paymentsApi.createPayment({
-      sourceId: 'ccof:GaJGNaZa8x4OgDJn4GB',
-      idempotencyKey: '7b0f3ec5-086a-4871-8f13-3c81b3875218',
+      sourceId: 'cnon:card-nonce-ok',
+      idempotencyKey: 'cd2e3872-32c2-4aae-856a-af8c274adfe3',
       amountMoney: {
         amount: 15,
         currency: 'USD'
-      },
-      autocomplete: true,
-      customerId: 'W92WH6P11H4Z77CTET0RNTGFW8',
-      locationId: 'LMJY97S69RNWK',
-      referenceId: '123456',
-      note: 'lakerise'
-    }); 
-  } catch(err){
-    console.log(err);
+      }
+    });
+  
+    console.log(response.result);
+  } catch(error) {
+    console.log(error);
   }
   });
 app.use(express.static(__dirname))
