306cf1ef1ba76a2cb392faf4f190a84158063f87
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-09-05 09:33:28 -0700

    remove try server

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ const client = new Client({
     environment: Environment.Production, // or Environment.Production
     accessToken: 'EAAAlvd0owabWAkzKXlgauXdesy8ahHbIdJFMODM7leb7Z_SdXJv1RaQ84ZeuGmz',
 });
-app.post('/create-payment', async(req, res) => {
-  const response = await client.paymentsApi.createPayment({
-    sourceId: 'cnon:card-nonce-ok',
-    idempotencyKey: 'cd2e3872-32c2-4aae-856a-af8c274adfe3',
-    amountMoney: {
-      amount: 15,
-      currency: 'USD'
-    }
+
+app.post('/create-payment', async (req, res) => {
+    const response = await client.paymentsApi.createPayment({
+      sourceId: 'cnon:card-nonce-ok',
+      idempotencyKey: 'cd2e3872-32c2-4aae-856a-af8c274adfe3',
+      amountMoney: {
+        amount: 15,
+        currency: 'USD'
+      }
+    });
+  
+    console.log(response.result);
+    res.status(200).json({ message: 'Payment created successfully' });
+
   });
-
-  // Process the payment with Square API
-  res.status(200).send(response.result);
-});
-
 app.use(express.static(__dirname))
 app.use(express.static("/create-payment"))
 app.listen(3000, () => {
