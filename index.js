8eb0139ab96d8b72eeab0c196570379235bc6bf2
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-09-05 10:24:53 -0700

    create payment function

--- index.js
+++ index.js
@@ -10,31 +10,76 @@ const client = new Client({
     accessToken: 'EAAAlvd0owabWAkzKXlgauXdesy8ahHbIdJFMODM7leb7Z_SdXJv1RaQ84ZeuGmz',
 });
 
-app.post('/api/app', async (req, res) => {
-  try {
-    const response = await client.paymentsApi.createPayment({
-      sourceId: 'ccof:GaJGNaZa8x4OgDJn4GB',
-      idempotencyKey: Date.now().toString(),
-      amountMoney: {
-        amount: 1500,
-        currency: 'USD'
-      },
-      appFeeMoney: {
-        amount: 0,
-        currency: 'USD'
-      },
-      autocomplete: true,
-      customerId: 'W92WH6P11H4Z77CTET0RNTGFW8',
-      locationId: 'LMJY97S69RNWK',
-      referenceId: '123456',
-      note: 'lakerise painting print'
-    });
-  
-    console.log(response.result);
-  } catch(error) {
-    console.log(error);
+async function createPayment(req, res) {
+  const payload = await json(req);
+  logger.debug(JSON.stringify(payload));
+  // We validate the payload for specific fields. You may disable this feature
+  // if you would prefer to handle payload validation on your own.
+  if (!validatePaymentPayload(payload)) {
+    throw createError(400, 'Bad Request');
   }
-    });
+
+  await retry(async (bail, attempt) => {
+    try {
+      logger.debug('Creating payment', { attempt });
+
+      const payment = {
+        idempotencyKey: payload.idempotencyKey,
+        locationId: payload.locationId,
+        sourceId: payload.sourceId,
+        // While it's tempting to pass this data from the client
+        // Doing so allows bad actor to modify these values
+        // Instead, leverage Orders to create an order on the server
+        // and pass the Order ID to createPayment rather than raw amounts
+        // See Orders documentation: https://developer.squareup.com/docs/orders-api/what-it-does
+        amountMoney: {
+          // the expected amount is in cents, meaning this is $1.00.
+          amount: '100',
+          // If you are a non-US account, you must change the currency to match the country in which
+          // you are accepting the payment.
+          currency: 'USD',
+        },
+      };
+
+      if (payload.customerId) {
+        payment.customerId = payload.customerId;
+      }
+
+      // VerificationDetails is part of Secure Card Authentication.
+      // This part of the payload is highly recommended (and required for some countries)
+      // for 'unauthenticated' payment methods like Cards.
+      if (payload.verificationToken) {
+        payment.verificationToken = payload.verificationToken;
+      }
+
+      const { result, statusCode } =
+        await square.paymentsApi.createPayment(payment);
+
+      logger.info('Payment succeeded!', { result, statusCode });
+
+      send(res, statusCode, {
+        success: true,
+        payment: {
+          id: result.payment.id,
+          status: result.payment.status,
+          receiptUrl: result.payment.receiptUrl,
+          orderId: result.payment.orderId,
+        },
+      });
+    } catch (ex) {
+      if (ex instanceof ApiError) {
+        // likely an error in the request. don't retry
+        logger.error(ex.errors);
+        bail(ex);
+      } else {
+        // IDEA: send to error reporting service
+        logger.error(`Error creating payment on attempt ${attempt}: ${ex}`);
+        throw ex; // to attempt retry
+      }
+    }
+  });
+}
+app.post('/api/app', createPayment);
   
   
 app.use(express.static(__dirname))
